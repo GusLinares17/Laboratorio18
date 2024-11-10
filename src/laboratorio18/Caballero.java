bceaa30353656937172ee01f903a9d7d8b284df1
Gustavo <glinares@unsa.edu.pe>
2024-11-10 18:23:34 -0500

    Clase Caballero completado

--- src/laboratorio18/Caballero.java
+++ src/laboratorio18/Caballero.java
@@ -4,10 +4,45 @@
  */
 package laboratorio18;
 
+import java.util.Random;
+
 /**
  *
  * @author Gustavo
  */
-public class Caballero {
-    
+public class Caballero extends Soldado {
+    private String arma;
+    private boolean montado;
+
+    public Caballero(String nombre) {
+        super(nombre, new Random().nextInt(3) + 3);
+        this.arma = "espada";
+        this.montado = false;
+    }
+    public String getArma() {
+        return arma;
+    }
+    public boolean estaMontado() {
+        return montado;
+    }
+    public void alternarArma() {
+        arma = arma.equals("espada") ? "lanza" : "espada";
+    }
+    public void montar() {
+        if (!montado) {
+            montado = true;
+            arma = "lanza";
+        }
+    }
+    public void desmontar() {
+        if (montado) {
+            montado = false;
+            arma = "espada";
+        }
+    }
+    public void envestir() {
+        int ataques = montado ? 3 : 2;
+        for (int i = 0; i < ataques; i++) {
+        }
+    }
 }
