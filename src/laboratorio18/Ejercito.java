1809e7eeac47ac06e0ce87353dd6a08ba072fea3
Gustavo <glinares@unsa.edu.pe>
2024-11-10 18:41:59 -0500

    Clase Ejercito version 2

--- src/laboratorio18/Ejercito.java
+++ src/laboratorio18/Ejercito.java
@@ -18,4 +18,29 @@ public class Ejercito {
         this.nombre = nombre;
         this.soldados = new ArrayList<>();
     }
+    private void generarSoldados() {
+        Random random = new Random();
+        int cantidadSoldados = random.nextInt(10) + 1;
+        for (int i = 0; i < cantidadSoldados; i++) {
+            int tipoSoldado = random.nextInt(3);
+            Soldado soldado;
+            switch (tipoSoldado) {
+                case 0 : soldado = new Espadachin(nombre + ":E" + (i + 1));
+                case 1 :  soldado = new Arquero(nombre + ":A" + (i + 1));
+                default : soldado = new Caballero(nombre + ":C" + (i + 1));
+            }
+            soldados.add(soldado);
+        }
+    }
+    public ArrayList<Soldado> getSoldados() {
+        return soldados;
+    }
+    public double promedioVida() {
+        return soldados.stream().mapToInt(Soldado::getVida).average().orElse(0);
+    }
+
+    public Soldado getSoldadoMayorVida() {
+        return soldados.stream().max((s1, s2) -> Integer.compare(s1.getVida(), s2.getVida())).orElse(null);
+    }
 }
+
