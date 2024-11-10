9b7ed35125ba26d0e78ef152d3ad505c473be74d
Gustavo <glinares@unsa.edu.pe>
2024-11-10 17:55:45 -0500

    Clase Soldado completada

--- src/laboratorio18/Soldado.java
+++ src/laboratorio18/Soldado.java
@@ -8,6 +8,21 @@ package laboratorio18;
  *
  * @author Gustavo
  */
+import java.util.Random.*;
 public class Soldado {
-    
+    String nombre;
+    int vida;
+    public Soldado(String nombre, int vida) {
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+    public int getVida() {
+        return vida;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void incrementarVida() {
+        this.vida += 1;
+    }
 }
